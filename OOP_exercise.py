f59166e777e22d7f21fb96a071c0949db2d17aa8
pavlekovacevic <kovacevicpavle14@gmail.com>
2022-01-12 04:17:14 -0800

    Hello

---
+++ OOP_exercise.py
@@ -0,0 +1,49 @@
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+car = Vehicle (1, 2)
+
+
+class vehicle:
+    pass
+
+
+class Bus(Vehicle):
+    pass
+
+
+class Bus(Vehicle):
+    def seating_capacity (self, capacity = 50):
+     
+        
+class Vehicle:
+    color = "White"
+
+
+
+
+class Vehicle:
+    def __init__(self, name, mileage, capacity):
+        self.name = name
+        self.mileage = mileage
+        self.capacity = capacity 
+
+    def fare (self):
+        return self.capacity * 100
+
+
+class Bus(Vehicle):
+    def fare(self):
+        amount = super().fare()
+        amount += amount * 10/100
+        return amount
+
+
+
+
+
+
+
+
